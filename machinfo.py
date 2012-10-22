0cf2a983fb296b05e248875cf12c6d6cdb2f99e2
Douglas Land <dsl@looprock.com>
2012-10-21 21:17:31 -0500

    changed UID generation method, added date as option to sample get scripts
    Moving from a MAC based unique ID to the hostname plus the MAC of eth0
    or uuidgen. Also added command line option of date to test get scripts

--- machinfo.py
+++ machinfo.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# FIXME: some things should be lists as output!
-
 # requires:
 # python-simplejson
 # http://svn.red-bean.com/bob/simplejson/tags/simplejson-1.3/docs/index.html
@@ -22,6 +20,7 @@ import subprocess
 from time import sleep, localtime, strftime
 from datetime import datetime
 from shlex import split
+import hashlib
 
 timeout = 5
 recdate = strftime("%d%m%Y", localtime())
@@ -43,6 +42,7 @@ def sanitize(txt):
         #txt = txt.replace(",", "&#44;")
         return txt
 
+# look through your path and see if a program exists, return the complete path if it does
 def which(program):
     import os
     def is_exe(fpath):
@@ -60,6 +60,7 @@ def which(program):
 
     return None
 
+# execute a command
 def runcom(command_line):
         start = datetime.now()
         process = subprocess.Popen(split(command_line), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
@@ -73,6 +74,7 @@ def runcom(command_line):
         out, error = process.communicate()
         return out.strip()
 
+# search for a string inside another string.
 def strchk(test,string):
 	p = re.compile(test)
 	result = re.search(p, string)
@@ -81,12 +83,7 @@ def strchk(test,string):
 	else:
 		return result
 
-def nettest(test,string):
-	if strchk(test, string) == 'T':
-		return string.split(test)[1].split()[0]
-	else:
-		return "Empty"
-
+# read a file and return it's contents
 def readfile(fname):
 	if os.path.exists(fname):
         	f = open(fname)
@@ -98,6 +95,8 @@ def readfile(fname):
 	else:
 		return None
 
+# generate nested python dictionaries, copied from here:
+# http://stackoverflow.com/questions/635483/what-is-the-best-way-to-implement-nested-dictionaries-in-python
 class AutoVivification(dict):
 	"""Implementation of perl's autovivification feature."""
 	def __getitem__(self, item):
@@ -107,7 +106,37 @@ class AutoVivification(dict):
 			value = self[item] = type(self)()
 			return value
 
+# create a fairly unique ID by creating a has from the hostname plus the MAC of eth0 or uuidgen
+def genuid(hostname):
+        data = runcom("ip addr show eth0")
+        for i in data.split("\n"):
+                r = re.search("link/ether", i)
+                if r:
+                        mac = i.split()[2]
+        # Trying to think if I've ever seen a system without an eth0; why take chances. 
+        # It's likely uuidgen + hostname will be unique enough..
+        if not mac:
+                mac = runcom("uuidgen")
+        return hashlib.md5(hostname + mac).hexdigest()
+
 sysdata = AutoVivification()
+
+# generate a unique ID for the system and cache it for future runs:
+hostname = runcom("uname -n")
+sysid = genuid(hostname)
+miconfdir = '/etc/machinfo'
+miconf = '%s/%s' % (miconfdir, 'id.txt')
+if os.path.exists(miconf):
+	sysid = readfile(miconf)
+else:
+	sysid = genuid(hostname)
+        if not os.path.exists(miconfdir):
+                os.mkdir(miconfdir)
+	f = open(miconf, 'w')
+	try:
+		f.write(sysid)
+	finally:
+		f.close()	
 
 # collect network information
 
@@ -116,31 +145,6 @@ sysdata = AutoVivification()
 data = runcom('ip addr show')
 p = re.compile(r'\n\d+: ')
 nets = p.split(data)
-
-# we're generating the unique ID for our system based on the first mac address and caching it after that
-miconfdir = '/etc/machinfo'
-miconf = '%s/%s' % (miconfdir, 'id.txt')
-if not os.path.exists(miconf):
-	if not os.path.exists(miconfdir):
-		os.mkdir(miconfdir)
-	for i in range(1,len(nets)):
-        	ifname = nets[i].split(':')[0]
-		# FIXME: bad hack to get around various virtual network devices, constructs, etc.
-		if re.search('eth', ifname):
-			if re.search('veth', ifname) == None:
-        			for x in nets[i].split('\n'):
-					s = re.compile(' link/ether ')
-					if re.search(s, x):
-						sysid = x.split()[1]
-						f = open(miconf, 'w')
-						try:
-							f.write(sysid)
-						finally:
-							f.close()
-						break
-else:
-	sysid = readfile(miconf)
-
 # for the below to work, I'm assuming lo will always be the first interface returned by ip addr show. Hopefully this is really the case.
 for i in range(1,len(nets)):
 	ifname = nets[i].split(':')[0]
@@ -185,7 +189,7 @@ for i in dp:
 
 # get timezone
 sysdata['timezone'] = runcom("/bin/date '+%Z'")
-sysdata['hostname'] = runcom("uname -n")
+sysdata['hostname'] = hostname
 
 # reverse lookups of interfaces
 lookups = []
@@ -270,11 +274,11 @@ for i in sorted(data):
 	sysdata['rpms'][n[0]]['version'] = n[1]
 	sysdata['rpms'][n[0]]['release'] = n[2]
 
-#print simplejson.dumps(sysdata, sort_keys=True, indent=2)
+print simplejson.dumps(sysdata, sort_keys=True, indent=2)
 url = "%s/machinfo/%s/%s" % (elasticsearch_url, recdate, sysid)
 data = simplejson.dumps(sysdata, sort_keys=True, indent=2)
 req = urllib2.Request(url, data, {'Content-Type': 'application/json'})
 f = urllib2.urlopen(req)
 response = f.read()
 f.close()
-#print response
+print response
